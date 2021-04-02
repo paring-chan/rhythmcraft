4e191a402cf76964ba08cdb7004761692585ad81
파랑이 <parangee9706@gmail.com>
2021-04-02 17:47:26 +0900

    feat: remove setting which makes setup hard

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 process.on('unhandledRejection', console.error)
 process.on('uncaughtException', console.error)
 
+
 // 기본 모듈
 const express = require('express');
 const http = require('http');
-const https = require('https');
 const passport = require('passport');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -16,6 +16,16 @@ const fs = require('fs');
 const path = require('path');
 const Url = require('url');
 const uniqueString = require('unique-string');
+
+const datadir = path.join(__dirname, 'data')
+
+const notesDir = path.join(datadir, 'notes')
+
+const avatarsDir = path.join(datadir, 'avatars')
+
+!fs.existsSync(notesDir) && fs.mkdirSync(notesDir)
+!fs.existsSync(avatarsDir) && fs.mkdirSync(avatarsDir)
+
 
 // 데이터베이스 스키마
 const User = require('./schemas/user');
@@ -44,7 +54,7 @@ connect();
 
 // SSL 관련 설정
 let options;
-if(setting.USE_SSL) {
+if (setting.USE_SSL) {
     options = {
         cert: fs.readFileSync(setting.SSL_CERT),
         key: fs.readFileSync(setting.SSL_KEY)
@@ -56,19 +66,18 @@ passport.serializeUser((user, done) => {
     done(null, user);
 });
 passport.deserializeUser((obj, done) => {
-    User.findOne({ snsID: obj.snsID , provider: obj.provider })
+    User.findOne({snsID: obj.snsID, provider: obj.provider})
         .then(async u => {
             const user = JSON.parse(JSON.stringify(u));
-            for(let key in user.equip) {
-                const item = await Item.findOne({ product_id : user.equip[key] });
-                const check_have = await Inventory.findOne({ owner : user.fullID , product_id : user.equip[key] });
-                if(!item || !check_have) {
+            for (let key in user.equip) {
+                const item = await Item.findOne({product_id: user.equip[key]});
+                const check_have = await Inventory.findOne({owner: user.fullID, product_id: user.equip[key]});
+                if (!item || !check_have) {
                     const equip = user.equip;
                     equip[key] = null;
-                    await User.updateOne({ fullID : user.fullID }, { equip });
+                    await User.updateOne({fullID: user.fullID}, {equip});
                     user.equip = equip;
-                }
-                else user[`equip_${key}`] = item.image_name;
+                } else user[`equip_${key}`] = item.image_name;
             }
             done(null, user);
         })
@@ -77,7 +86,7 @@ passport.deserializeUser((obj, done) => {
 
 // 세션, REDIS
 let sessionMiddleware;
-if(setting.USE_REDIS) {
+if (setting.USE_REDIS) {
     const client = redis.createClient({
         host: setting.REDIS_HOST,
         port: setting.REDIS_PORT,
@@ -88,14 +97,13 @@ if(setting.USE_REDIS) {
         secret: setting.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
-        store: new RedisStore({ client: client }),
+        store: new RedisStore({client: client}),
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7
         }
     })
     app.use(sessionMiddleware);
-}
-else {
+} else {
     sessionMiddleware = session({
         secret: setting.SESSION_SECRET,
         resave: false,
@@ -119,10 +127,10 @@ app.use(passport.session());
 
 // 정적 파일 제공
 const staticoptions = {
-    index: setting.INDEX
+    index: [ "index.htm" , "index.html" ]
 }
 app.use(express.static(__dirname + "/public/", staticoptions));
-app.use('/avatar', express.static(path.join(setting.AVATAR_PATH), staticoptions));
+app.use('/avatar', express.static(avatarsDir, staticoptions));
 
 // view engine을 EJS로 설정
 app.set('views', './views');
@@ -135,7 +143,7 @@ fs.readdirSync('./login').forEach((file) => {
 
 // IE 경고
 app.use((req, res, next) => {
-    if(/trident|msie/gi.test(req.get('User-Agent'))) {
+    if (/trident|msie/gi.test(req.get('User-Agent'))) {
         req.flash('Warn', 'IE는 정상 작동을 보장하지 않습니다. <a href="https://www.google.com/chrome/">Chrome</a>, <a href="https://www.mozilla.org/ko-KR/firefox/new/">FireFox</a>, <a href="https://whale.naver.com/ko/download">Whale</a> 등의 최신 브라우저를 이용해주세요.');
     }
     next();
@@ -143,7 +151,7 @@ app.use((req, res, next) => {
 
 // 벤 감지
 app.use((req, res, next) => {
-    if(req.isAuthenticated() && req.user.block_login >= Date.now()) {
+    if (req.isAuthenticated() && req.user.block_login >= Date.now()) {
         req.flash('Error', `관리자에 의해 계정이 정지되어 ${new Date(req.user.block_login).toLocaleDateString()} ${new Date(req.user.block_login).toLocaleTimeString()}까지 로그인이 불가능합니다.<br>계정 정지 사유 : ${req.user.block_login_reason || '사유가 지정되지 않음'}`);
         req.logout();
         return res.redirect('/login');
@@ -153,7 +161,7 @@ app.use((req, res, next) => {
 
 // 닉네임 설정하지 않은 유저 닉네임 설정시키기
 app.use((req, res, next) => {
-    if(req.isAuthenticated() && !req.user.nick_set && req.url != '/mypage' && req.url != '/editaccount') return res.redirect('/mypage');
+    if (req.isAuthenticated() && !req.user.nick_set && req.url != '/mypage' && req.url != '/editaccount') return res.redirect('/mypage');
     next();
 });
 
@@ -187,9 +195,9 @@ app.use((req, res, next) => {
 
 // 클라이언트 인식
 app.use((req, res, next) => {
-    if(req.query.isClient == 'true') req.session.isClient = true;
-    if(req.query.isClient == 'false') req.session.isClient = false;
-    if(req.session.isClient && !req.isAuthenticated() && !req.url.startsWith('/login') && !req.url.startsWith('/getqrcode')) return res.redirect('/login');
+    if (req.query.isClient == 'true') req.session.isClient = true;
+    if (req.query.isClient == 'false') req.session.isClient = false;
+    if (req.session.isClient && !req.isAuthenticated() && !req.url.startsWith('/login') && !req.url.startsWith('/getqrcode')) return res.redirect('/login');
     next();
 });
 
@@ -202,17 +210,9 @@ fs.readdirSync('./routes').forEach((file) => {
 console.log('라우터를 모두 불러왔습니다.\n');
 
 // 서버 구동
-let server;
-if(setting.USE_SSL) {
-    server = https.createServer(options, app).listen(setting.PORT, () => {
-        console.log('보안 서버가 구동중입니다!');
-    });
-}
-else {
-    server = http.createServer(app).listen(setting.PORT, () => {
-        console.log("서버가 구동중입니다!");
-    });
-}
+let server = http.createServer(app).listen(setting.PORT, () => {
+    console.log("서버가 구동중입니다!");
+})
 
 webSocket(server, app, sessionMiddleware);
 
@@ -224,19 +224,22 @@ setImmediate(async () => {
 });
 
 async function CreateOfficialRoom() {
-    const count = await File.countDocuments({ public : true , file_type : 'note' });
-    const note = await File.findOne({ public : true , file_type : 'note' }).skip(utils.getRandomInt(0, count - 1));
+    const count = await File.countDocuments({public: true, file_type: 'note'});
+    const note = await File.findOne({public: true, file_type: 'note'}).skip(utils.getRandomInt(0, count - 1));
 
     let token_result;
     let note_file = String(fs.readFileSync(path.join(setting.SAVE_FILE_PATH, note.name)));
-    if(path.extname(note.name) == '.signedrhythmcraft') {
+    if (path.extname(note.name) == '.signedrhythmcraft') {
         token_result = utils.verifyToken(note_file);
         if (token_result.error) return CreateOfficialRoom();
-    }
-    else note_file = JSON.parse(note_file);
+    } else note_file = JSON.parse(note_file);
 
-    const music = await File.findOne({ name : token_result != null ? token_result.music : note_file.music , public : true , file_type : 'music' });
-    if(!music) return CreateOfficialRoom();
+    const music = await File.findOne({
+        name: token_result != null ? token_result.music : note_file.music,
+        public: true,
+        file_type: 'music'
+    });
+    if (!music) return CreateOfficialRoom();
 
     await Room.create({
         name: "자동 진행 공식 방",
@@ -253,6 +256,6 @@ async function CreateOfficialRoom() {
 }
 
 setInterval(async () => {
-    await Chat.deleteMany({ createdAt : { $lt : Date.now() - 259200000 } , reported : false });
-    await Promotion.deleteMany({ expires : { $lt : Date.now() } });
+    await Chat.deleteMany({createdAt: {$lt: Date.now() - 259200000}, reported: false});
+    await Promotion.deleteMany({expires: {$lt: Date.now()}});
 }, 60000);
