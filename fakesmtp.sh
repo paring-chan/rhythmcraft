469f86d62615a7d286e7fc0d01f6285fe90ce7a2
파랑이 <parangee9706@gmail.com>
2021-04-02 18:12:00 +0900

    feat: remove fakesmtp.sh

--- fakesmtp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p dev/fake-smtp
-cd dev/fake-smtp
-if [ ! -f *.jar ]; then
-  wget -O smtp.zip http://nilhcem.github.com/FakeSMTP/downloads/fakeSMTP-latest.zip
-  unzip smtp.zip
-fi
-
-java -jar *.jar -p 2525 -s
